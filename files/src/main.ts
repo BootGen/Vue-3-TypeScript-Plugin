b15f1b7ff8adb4da27c61555e048d618b23debd7
Gabor Angyal <angyalgabor@outlook.com>
2021-07-27 13:00:56 +0200

    Vue 3 upgrade

--- files/src/main.ts
+++ files/src/main.ts
@@ -1,31 +1,6 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify'
-import { User } from './models/User'
 
-Vue.config.productionTip = false
-
-store.dispatch('init');
-
-Vue.filter('dateFormat', function(date: Date){
-  return `${date.toLocaleDateString('en-US')}, ${date.toLocaleTimeString('en-US')}`;
-})
-
-new Vue({
-  router,
-  store,
-  vuetify,
-  mounted: async function() {
-    if(store.state.auth.jwt){
-      this.$store.state.auth.user = await this.$store.dispatch('profile');
-    }
-  },
-  data: function () {
-    return {
-      user: null as (User | null)
-    };
-  },
-  render: h => h(App)
-}).$mount('#app')
+createApp(App).use(store).use(router).mount('#app')
