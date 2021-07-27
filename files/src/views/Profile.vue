d1998ee282ca88232a3e2cc9010b2e7784baf071
Gabor Angyal <angyalgabor@outlook.com>
2021-07-27 13:44:11 +0200

    login and profile view

---
+++ files/src/views/Profile.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container-fluid">
+    <div class="mb-3" v-if="store.state.auth.user">
+        <p>
+            <strong> user name </strong>
+            {{ store.state.auth.user.userName }}
+        </p>
+    </div>
+    <div class="mb-3" v-if="store.state.auth.user">
+        <p>
+            <strong> email </strong>
+            {{ store.state.auth.user.email }}
+        </p>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { useStore } from 'vuex';
+import { State } from '../store';
+import { defineComponent } from 'vue';
+export default defineComponent({
+    setup() {
+        const store = useStore<State>();
+        return { store }
+    },
+})
+</script>
